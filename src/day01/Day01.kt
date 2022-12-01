e5c2c3653f9ee44e78b3b48ca4bf4e2daccaf00f
IThinkIGottaGo <llehnevaeh@vip.qq.com>
2022-12-01 20:13:35 +0800

    tweak day01 format

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -3,30 +3,28 @@ package day01
 import groupBySkipNullKey
 import readInput
 
+var elfIndex = 0
+
 fun main() {
     fun part1(input: List<String>): Int {
-        var elfIndex = 0
-        val results = input.groupBySkipNullKey({ calories ->
-            calories.ifEmpty {
-                ++elfIndex
-                null
-            }?.let { elfIndex }
-        }, String::toInt, { 0 }) { group, value ->
-            group + value
-        }
+        elfIndex = 0
+        val results = input.groupBySkipNullKey(
+            ::elfIndexOrNull,
+            String::toInt,
+            ::groupInt,
+            Int::plus
+        )
         return results.values.maxOrNull()!!
     }
 
     fun part2(input: List<String>): Int {
-        var elfIndex = 0
-        val results = input.groupBySkipNullKey({ calories ->
-            calories.ifEmpty {
-                ++elfIndex
-                null
-            }?.let { elfIndex }
-        }, String::toInt, { 0 }) { group, value ->
-            group + value
-        }.values.sortedDescending()
+        elfIndex = 0
+        val results = input.groupBySkipNullKey(
+            ::elfIndexOrNull,
+            String::toInt,
+            ::groupInt,
+            Int::plus
+        ).values.sortedDescending()
         return results.take(3).sum()
     }
 
@@ -36,6 +34,14 @@ fun main() {
     check(part2(testInput) == 45000)
 
     val input = readInput("day01")
-    println(part1(input))
-    println(part2(input))
+    println(part1(input))   // 72718
+    println(part2(input))   // 213089
 }
+
+fun elfIndexOrNull(calories: String): Int? = calories.ifEmpty {
+    ++elfIndex
+    null
+}?.let { elfIndex }
+
+
+fun groupInt() = 0
